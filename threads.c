364238b33d5475a84fd7c0240bac0b2c0f8e352a
Christian Hidalgo <chidalgo088@gmail.com>
2018-02-04 14:27:36 -0500

    Update threads.c
    Added the preprocessor commands in order to work from the makefile, added comments.

--- threads.c
+++ threads.c
@@ -1,34 +1,39 @@
-#include <stdio.h>
-#include <pthread.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
+#include "threads.h"
 
-//-------------------
-void* SimpleThread();
-int isDigit(char);
-int validateInput(char*[], int);
-int checkCommand(char*);
-//-------------------
+//Program Description: threads.c executes any number of threads. Each thread
+//will access a global shared variable and will add to it.
+//Program will wait until all threads are done.
+//Compilation: make build (This will prompt the user if they want to define PTHREAD_SYNC for
+//syncronization)
+
+
+
+//--------Global Variables----------
+
 int SharedVariable = 0; 
 pthread_barrier_t threadBarrier;
 pthread_mutex_t threadMutex = PTHREAD_MUTEX_INITIALIZER;
-int PTHREAD_SYNC = 0;
-//-------------------
+
+//----------MAIN---------
 
 int main( int argc, char *argv[]){
 	
 
-	char *c;// this is basically a string
-	c = argv[1];// what is in here is a sequence of strings
+
+	char *c;	// this is basically a string
+	c = argv[1];	// what is in here is a sequence of strings
 	
 	int result = validateInput(argv, argc);
 
 	if(result == 0){
-		printf("Please enter ONE NUMBER greater than 0\n ");
+		printf("\n-----------------ERROR-----------------\n");
+		printf("Please enter ONE NUMBER greater than 0\n");
+		printf("---------------------------------------\n");
 		return 0;
-	}
-	
+	}//if validation fails, program exits with a 0.
+
+	printf("\n--------------------------------------\n");
+
 	int barrierStatus = pthread_barrier_init(&threadBarrier, NULL, result);
 
 	pthread_t myThread[result];	
@@ -39,13 +44,13 @@ int main( int argc, char *argv[]){
 	 	threads[i] = i;
 		pthread_create(&myThread[i], NULL, SimpleThread, &threads[i] );
 
-	}// end of for
+	}
 
 	for (int i=0; i < result; i++){
 
 		pthread_join(myThread[i], NULL);
 
-	}
+	}//program waits for all threads to finish prior exiting.
 
 	printf("\n-----------------------------\n");
 	printf("*** The Program has Ended ***\n");
@@ -54,7 +59,7 @@ int main( int argc, char *argv[]){
 }
 
 //*************************************************************************
-//validadeInput() takes in 2 inputs, 'input' is the value taken from the 
+//validadeInput() takes in 2 inputs, 'argv[]' is the value taken from the 
 //command line. 'argc' is the number of arguments.
 //(i.e. COMMAND LINE = ./program 32 | input = '32' and argc = 2)
 //It will iterate thru the string and check if the char is a digit.
@@ -64,14 +69,8 @@ int validateInput(char* argv[], int argc){
 
 	char* input = argv[1];
 	char* status = "";
-	int indicator;
 
-	if(argc == 3){
-		char* command = argv[2];
-		indicator = checkCommand(command);
-	}
-
-	if(argc > 3 || indicator == -1)
+	if(argc > 2)
 		return 0;
 
 	int length = strlen(input);
@@ -83,18 +82,7 @@ int validateInput(char* argv[], int argc){
 
 		if( isDigit(temp) == 0 )
 			return 0;
-	}
-
-	if(indicator == 1){
-		status = "SYNC";	
-	}else{
-		status = "NORMAL";
-	}
-
-	printf("-------------------------------\n");
-	printf("*** Program mode: %s ***\n", status);
-	printf("-------------------------------\n");
-	sleep(2);
+	}//this loop checks each char to see if its a digit (0-9)
 
 	int result = atoi((char*)input);
 
@@ -102,31 +90,11 @@ int validateInput(char* argv[], int argc){
 
 }
 
-int checkCommand(char* command){
-	
-	if(command[0] != '-'){
-		return -1;
-	}
-	else if(command[0]=='-' && command[1]=='s' && strlen(command)==2){
-		PTHREAD_SYNC = 1;
-	}
-	else{
-
-		printf("\n**NOTE: %s is NOT a valid command.\n", command);
-		printf("Program will execute in 'normal' mode.\n\n");
-		sleep(3);
-		return 0;
-	}
-
-	
-	return 1;			
-
-}
 
 //*************************************************************************
-//isDigit() takes in 1 input, 'position'
-//command line. 'argc' is the number of arguments.
-//(i.e. COMMAND LINE = ./program 32 | input = '32' and argc = 2)
+//isDigit() takes in 1 input, 'position' and it wil check if the char in
+//that position is a digit (0-9). Returns a 1 if its a digit, otherwise
+//returns 0	
 //*************************************************************************
 int isDigit(char position)
 {
@@ -141,7 +109,8 @@ int isDigit(char position)
 }
 
 //*************************************************************************
-
+// This is the function that will be executed by the threads.
+// if PTHREAD_SYNC is defined it will sync the threads, otherwise it wont.
 //*************************************************************************
 void* SimpleThread (int* which){
 
@@ -156,24 +125,26 @@ void* SimpleThread (int* which){
 		if(random()> RAND_MAX/2)
 			usleep(500);
 
-		if(PTHREAD_SYNC == 1)
+		#ifdef PTHREAD_SYNC
 			pthread_mutex_lock(&threadMutex);
+		#endif
 	
 		val = SharedVariable;
 		printf("*** thread %d sees value %d\n", TID, val);
 		SharedVariable = val +1 ;
 		count++;	
 
-		if (PTHREAD_SYNC == 1)
+		#ifdef PTHREAD_SYNC
 			pthread_mutex_unlock(&threadMutex);
+		#endif
 	
 	}
 
-	if (PTHREAD_SYNC == 1)
+	#ifdef PTHREAD_SYNC
 		pthread_barrier_wait(&threadBarrier);
+	#endif
 	
 	val = SharedVariable;
 	printf("Thread %d sees final value %d\n", TID, val);
-	//printf("Thread %d count: %d\n", TID, count);
 
 }
